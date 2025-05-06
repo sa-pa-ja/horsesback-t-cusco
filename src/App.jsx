f3546917f6085e6c79a4bc146bd381cd7cd01c35
saturnino <161881@unsaac.edu.pe>
2025-05-06 14:20:28 -0500

    You can also directly specify the project name and the template you want to use via additional command line options

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <p>Hello</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
